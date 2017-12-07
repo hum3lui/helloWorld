a6943bf4f881506e2231fbb50e08c4365063d2b1
humphrey.lui <hum3lui@gmail.com>
2017-12-07 13:58:57 +0800

    say hi 3 times

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,8 @@
-public class Hello{
+public class helloWorld{
 	
 	public static void main(String[] args){
 		
-		for(i=1;i<4;i++){
+		for(int i=1;i<4;i++){
 			System.out.println("Hello World! " + i);
 		}	
 	}	
